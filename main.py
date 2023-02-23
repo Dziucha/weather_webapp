a90140e6de4878c76f4523800a51f6cf4cbb43ec
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-23 18:25:58 +0100

    interactive plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(layout="centered")
 
@@ -17,3 +18,16 @@ if place != "":
     else:
         info = st.subheader(f"{data_to_view} for the "
                             f"next {days} days in {place}")
+
+
+def get_data(days_local):
+    dates_local = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures_local = [10, 11, 15]
+    temperatures_local = [days_local * i for i in temperatures_local]
+    return dates_local, temperatures_local
+
+
+dates, temperatures = get_data(days)
+figure = px.line(x=dates, y=temperatures,
+                 labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
