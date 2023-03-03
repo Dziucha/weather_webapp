d74d1f4907a7899ec86845bcc7c04aa2fd18b105
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-03-03 19:20:17 +0100

    sky and temperature conditions, error exception

--- backend.py
+++ backend.py
@@ -3,12 +3,14 @@ import requests
 API_KEY = "4b63b6ab44b802032a14d74405afbe71"
 
 
-def get_data(place, forecast_days=None, type_of_data=None):
+def get_data(place, forecast_days):
     url = "http://api.openweathermap.org/data/2.5/" \
-          f"forecast?q={place}&appid={API_KEY}"
+          f"forecast?q={place}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    filtered_data = filtered_data[:8 * forecast_days]
+    return filtered_data
 
 
 if __name__ == "__main__":
